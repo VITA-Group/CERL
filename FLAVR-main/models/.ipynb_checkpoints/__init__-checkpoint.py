254ae324503ed85cb0517d645732fa45563209d0
zychen <53959668+zychen-ustc@users.noreply.github.com>
2021-10-10 18:36:53 +0800

    Delete FLAVR-main directory

--- FLAVR-main/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from .models import register, make
-from . import mlp
-from . import misc
-from . import liif_den
-from . import liif_optical
-# from . import liif_ode
-from . import liif_gtoptical
--- FLAVR-main/ops/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# phoenixyli 李岩 @2020-04-02 14:29:04
-from ops.basic_ops import *
